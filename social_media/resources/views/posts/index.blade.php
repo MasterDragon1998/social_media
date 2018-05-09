90a0573244f5cd513146b461fe534f751bd81351
MasterDragon1998 <d.raangs@gmail.com>
2018-05-09 16:28:21 +0200

    Revert "Remade /posts page"
    This reverts commit 5863195dd46b84f74d43e6a57dd99b2e0f277937.

--- social_media/resources/views/posts/index.blade.php
+++ social_media/resources/views/posts/index.blade.php
@@ -4,14 +4,9 @@
 	<h1>Posts</h1>
 	@if(count($posts) > 0)
 		@foreach($posts as $post)
-			<div style="margin:5px;" class="card">
-				<div class="card-header">
-					<h2><a href="{{url('/posts/'.$post->id)}}">{{$post->title}}</a></h2>
-				</div>
-				<div class="card-footer">
-					<small>Created at: {{$post->created_at}}</small>
-					<small>Created by: {{$post->user->name}}</small>
-				</div>
+			<div>
+				<h2><a href="{{url('/posts/'.$post->id)}}">{{$post->title}}</a></h2>
+				<small>Created at: {{$post->created_at}}</small>
 			</div>
 		@endforeach
 		{{$posts->links()}}
